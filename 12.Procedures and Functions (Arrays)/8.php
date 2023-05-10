678d13aaf33eee5cae3351d9af7ad262393f799f
Kirill <k.akshentsev04@mail.ru>
2023-05-10 20:59:02 +0500

    12th module last tasks complete

--- 12.Procedures and Functions (Arrays)/8.php
+++ 12.Procedures and Functions (Arrays)/8.php
@@ -10,5 +10,24 @@
 */
 
 function SortInc(&$A, $N) {
+    $min_index = 0;
+    $min = PHP_INT_MAX;
+    for ($i = 0; $i < $N; $i++) {
+        for ($j = $i; $j < $N; $j++) {
+            if($min > $A[$j]) {
+                $min = $A[$j];
+                $min_index = $j;
+            }
+        }
+        $tmp = $A[$i];
+        $A[$i] = $min;
+        $A[$min_index] = $tmp;
 
+        $min_index = 0;
+        $min = PHP_INT_MAX;
+    }
 }
+
+$arr = [4, 3, 10, 23, -100, 2, 1];
+SortInc($arr, count($arr));
+print_r($arr);
